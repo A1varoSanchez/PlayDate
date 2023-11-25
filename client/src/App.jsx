a5c2cfe49c4aa6a38e37c2deef6c94a0cf1ac9c3
Byron Alava <braceddrago3@gmail.com>
2023-11-25 14:45:28 +0100

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,21 @@
-import { useState } from 'react'
+
 import Navigation from './components/Navigation/Navigation'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
 
   return (
 
-<<<<<<< HEAD
       <div className='App'>
 
         <Navigation />
 
         <AppRoutes />
 
-        <Footer />
+        
          
       </div>
-=======
-    <div className="App">
-
-      <Navigation />
-
-      <h1>PlayDates está en fase de desarrollo...</h1>
-
-
-    </div>
-
->>>>>>> 412173ad1370101265ccaf1df284ae8e9c05a35c
   )
 }
 
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 
-import SignupPage from '../pages/SignupPage/SignupPage.jsx'
-import LoginPage from '../pages/LoginPage/LoginPage.jsx'
+
 
 const AppRoutes = () => {
 
@@ -9,8 +8,8 @@ const AppRoutes = () => {
         <Routes>
             <Route path={'/'} element={<p>EL INICIO</p>} />
            
-            <Route path={'/registro'} element={<SignupPage />} />
-            <Route path={'/inicio-sesion'} element={<LoginPage />} />
+            
+        
 
             <Route path={'*'} element={<p>EL ERROR</p>} />
         </Routes>
