831746e22a5d9a4a5affe67c778555f640146f5b
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-11-25 14:14:59 +0100

    model

--- server/models/Event.model.js
+++ server/models/Event.model.js
@@ -1,40 +1,45 @@
 const { Schema, model } = require("mongoose")
 
 
-const eventSchema = new Schema({
-
-    name: {
-        type: String,
-        required: [true, 'El nombre es obligatorio.']
-    },
-
-    type: {
-        type: String,
-        required: [true, 'El tipo es obligatorio.']
-    },
-
-    description: {
-        type: String,
-        minlength: 0
-    },
-
-    location: {                                           // atencion
-        type: {
-            type: String
+const eventSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'El nombre es obligatorio.']
         },
-        coordinates: {
-            type: [Number]
-        }
+        type: {
+            type: String,
+            required: [true, 'El tipo es obligatorio.']
+        },
+        description: {
+            type: String,
+            minlength: 0
+        },
+        location: {                                           // atencion
+            type: {
+                type: String
+            },
+            coordinates: {
+                type: [Number]
+            }
+        },
+        ageGroup: {
+            type: Number,
+            enum: ['0-3', '3-6', '6-9', '2-5', '5-8', '8-11', '10-13'],
+            required: [true, 'Selecciona rango de edad.']
+        },
+        messages: [{
+            type: String,
+            sender: {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        }],
     },
-
-    ageGroup: {
-        type: Number,
-        enum: ['0-3', '3-6', '6-9', '2-5', '5-8', '8-11', '10-13'],
-        required: [true, 'Selecciona rango de edad.']
+    {
+        timestamps: true
     }
-
-
-})
+)
 
 const Event = model("Event", eventSchema)
 
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,57 +1,53 @@
 const { Schema, model } = require("mongoose")
 
 
-const userSchema = new Schema({
-
-  email: {
-    type: String,
-    required: [true, 'Email is required.'],
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-
-  password: {
-    type: String,
-    required: [true, 'Password is required.']
-  },
-
-  username: {
-    type: String,
-    required: [true, 'Username is required.'],
-    minlength: [3, 'Username must be at least 3 characters.']
-  },
-
-  children: [{
-    gender: {
-      enum: [boy, girl]
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true
     },
-    birth: {
-      type: Date,
+    password: {
+      type: String,
+      required: [true, 'Password is required.']
     },
-  }],
+    username: {
+      type: String,
+      required: [true, 'Username is required.'],
+      minlength: [3, 'Username must be at least 3 characters.']
+    },
+    children: [{
+      gender: {
+        enum: [boy, girl]
+      },
+      birth: {
+        type: Date,
+      },
+    }],
+    family_type: {
+      type: String,
+    },
+    photo: {
+      type: String
+    },
+    friends: {
+      type: Array
+    },
+    role: {
+      type: String,
+      enum: ['USER', 'ADMIN'],
+      default: 'USER'
+    },
 
-  family_type: {
-    type: String,
   },
+  {
+    timestamps: true
+  }
+)
 
-  photo: {
-    type: String
-  },
-
-  friends: {
-    type: Array
-  },
-
-  role: {
-    type: String,
-    enum: ['USER', 'ADMIN'],
-    default: 'USER'
-  },
-
-  timestamps: true
-
-})
 
 const User = model("User", userSchema)
 
