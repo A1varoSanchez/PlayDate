2512c83722c1721b96e0f93c42fef7fb245db9a4
Chiara Manni <chiaramiren@gmail.com>
2023-11-26 18:05:21 +0100

    footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer style={{ backgroundColor: "black", color: "white", padding: "10px", textAlign: "center" }}>
+            Todos los derechos reservados - Chalby
+        </footer>
+    )
+}
+
+export default Footer
